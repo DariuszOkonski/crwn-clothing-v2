2eb53d35d281d9cd14555dfe3b301bb4758254e7
Dariusz Okoński <darek200180@gmail.com>
2023-07-02 20:58:31 +0200

    build basics routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import { Outlet, Route, Routes } from 'react-router-dom';
 import Home from './components/routes/home/home.component';
 
+const Shop = () => {
+  return <h1>I am the Shop page</h1>;
+};
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>I am the navigation bar</h1>
+      </div>
+      <Outlet />
+    </div>
+  );
+};
+
 const App = () => {
-  return <Home />;
+  return (
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
